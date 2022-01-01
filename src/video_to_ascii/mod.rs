fbe3f31439d30f71cb39c0750db238f0e41be201
Brighton Sikarskie <brighton.sikarskie@tuta.io>
2022-01-01 03:36:26 -0600

    done for now

--- src/video_to_ascii/mod.rs
+++ src/video_to_ascii/mod.rs
@@ -1,10 +1,9 @@
-use std::{sync::atomic::{AtomicUsize, Ordering}, io::Write};
+use std::{io::Write, process::Command};
 
 use image::{
     imageops::{resize, FilterType},
     GenericImageView,
 };
-use rayon::prelude::*;
 use terminal_size::terminal_size;
 
 mod ascii_image;
@@ -66,41 +65,38 @@ impl<'a> AsciiVideo<'a> {
     }
 
     fn file_output(&mut self) {
-        let i = AtomicUsize::new(0);
-        self.video.iter().par_bridge().for_each(|frame| {
-            let cnt = i.fetch_add(1, Ordering::Relaxed);
+        let dir = ".video_to_ascii_tmp";
+        let prefix = self.output.unwrap().split('.').next().unwrap();
+        if !std::path::Path::new(dir).exists() {
+            std::fs::create_dir(dir).unwrap();
+        }
+
+        for (cnt, frame) in self.video.iter().enumerate() {
             let img = frame.as_rgb().unwrap();
-            let width_and_height =
-                Self::width_and_height(self.width, self.height, self.output, &img);
-            let img = match width_and_height {
+            let img = match Self::width_and_height(self.width, self.height, self.output, &img) {
                 Some((width, height)) => resize(&img, width, height, self.filter),
                 None => img,
             };
 
-            let ascii_image = AsciiImage::new(img);
+            let path = format!("{}/{}_{}.png", dir, prefix, cnt);
 
-            let prefix = self.output.unwrap().split('.').next().unwrap();
-            let extension = self.output.unwrap().split('.').last().unwrap();
-            // make dir
-            let dir = ".video_to_ascii_tmp";
-            if !std::path::Path::new(dir).exists() {
-                std::fs::create_dir(dir).unwrap();
-            }
-            let path = format!("{}/{}_{}.{}", dir, prefix, cnt, extension);
-
-            print!("{}/{}\r", cnt, self.total_frames);
+            print!("{} / {}\r", cnt, self.total_frames);
             std::io::stdout().flush().unwrap();
 
-            ascii_image.output_to_file(path, self.px);
+            AsciiImage::new(img).output_to_file(path, self.px);
+        }
+        // save all files to mp4 and remove each file one added to the mp4
+        // TODO make this work without Command
+        Command::new("ffmpeg")
+            .args(&[
+                "-i",
+                &format!("{}/{}_%d.png", dir, prefix),
+                &format!("{}", self.output.unwrap()),
+            ])
+            .output()
+            .expect("failed to execute process");
 
-            // save all files to mp4 and remove each file one added to the mp4
-
-            // remove dir if it's empty
-            // this should be empty because all the files should be removed
-            if std::fs::read_dir(dir).unwrap().count() == 0 {
-                std::fs::remove_dir(dir).unwrap();
-            }
-        });
+        std::fs::remove_dir_all(dir).unwrap();
     }
 
     fn stdout_output(&mut self) {
